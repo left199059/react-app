997759e4a334f9517bf5a530a91aa06c4245b69d
曹康 <ckya@caokangdeMacBook-Pro.local>
2017-06-07 23:53:47 +0800

    3-1

--- src/components/common/phoneNumberInput.js
+++ src/components/common/phoneNumberInput.js
@@ -5,16 +5,22 @@ class PhoneNumberInput extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      blur: true,
+      blur: false,
     };
     this.handleInput = this.handleInput.bind(this);
     this.clearInput = this.clearInput.bind(this);
+    this.iconToggle = this.iconToggle.bind(this);
   }
   handleInput(e) {
     this.props.phoneInputHandle(e.target.value.trim().replace(/\D/g, ''));
   }
   clearInput() {
     this.props.phoneInputHandle('');
+  }
+  iconToggle() {
+    this.setState((prevState, props) => ({
+      blur: props.phoneNumber === '' ? prevState.blur : !prevState.blur,
+    }));
   }
   render() {
     return (
@@ -25,10 +31,10 @@ class PhoneNumberInput extends Component {
           maxLength="11"
           value={this.props.phoneNumber}
           onInput={this.handleInput}
-          onBlur={input => (input.state = !input.state)}
-          onFocus={input => (input.state = !input.state)}
+          onBlur={this.iconToggle}
+          onFocus={this.iconToggle}
         />
-        <span className={`${this.props.phoneNumber === '' ? 'mui-hidden' : ''} mui-icon mui-icon-clear`}
+        <span className={`${(this.props.phoneNumber === '' || this.state.blur) ? 'mui-hidden' : ''} mui-icon mui-icon-clear`}
           onTouchTap={this.clearInput}
         />
       </div>
